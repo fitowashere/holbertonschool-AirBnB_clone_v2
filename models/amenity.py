9293c1c80593f70fc9d201c646df9a557ac42a50
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-11-30 13:56:18 -0400

    library import changed to see if that is the problem

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
 from models.base_model import Base
+from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
@@ -16,7 +16,7 @@ class Amenity(BaseModel, Base):
         name (sqlalchemy String: The Amenity name
         place_amenities (sqlalchemt relationship): Place-Amenity relationship.
     """
-    __tablename__ = 'amenities'
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity",
                                     viewonly=False)
