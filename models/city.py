64d50ae7b3536b6bf4eccaba1da4a5082f9771d2
GreyLeaf23 <christian.torres2894@gmail.com>
2023-11-29 21:14:57 -0400

    Task 6 - made documentation to city.py

--- models/city.py
+++ models/city.py
@@ -2,9 +2,7 @@
 """Defines the City class."""
 from models.base_model import Base
 from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
@@ -18,7 +16,12 @@ class City(BaseModel, Base):
         name (sqlalchemy String): The name of the City.
         state_id (sqlalchemy String): The state id of the City.
     """
+    # Task 6 (Start)
     __tablename__ = "cities"
+    # Add new column to database called name.
     name = Column(String(128), nullable=False)
+    # Add new column to database called state_id.
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    # Task 6 (End)
+    # Add new relationship to database called places.
     places = relationship("Place", backref="cities", cascade="all, delete")
