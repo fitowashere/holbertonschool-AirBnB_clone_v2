b61a6b465e301899d779798b76dd136d2909741c
GreyLeaf23 <christian.torres2894@gmail.com>
2023-11-29 20:32:17 -0400

    Task 5 - update __init__.py in models folder.

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-"""Instantiates a storage object.
-
--> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
-   instantiates a database storage engine (DBStorage).
--> Otherwise, instantiates a file storage engine (FileStorage).
 """
-from os import getenv
+This module instantiates an object of class FileStorage
+and DBStorage, depending on the value of the environment.
+"""
 
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
+import os
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+
+# Task 6 (Start)
+
+# If the environment variable HBNB_TYPE_STORAGE is equal to db,
+# create an instance of DBStorage and store it in the variable storage.
+if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
     storage = DBStorage()
+# Else, create an instance of FileStorage and store it in the variable storage.
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
+
 storage.reload()
+
+# Task 6 (End)
