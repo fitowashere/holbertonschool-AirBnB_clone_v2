408cb51679c5df27ee4b128e2a859f25efcf49fe
fitowashere <fito_washere@icloud.com>
2023-12-14 10:55:37 -0400

    fixed syntax error on line 23

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """ Create a route of /c"""
-    return 'C {}.format(text.replace('_', ' '))
+    return 'C {}'.format(text.replace('_', ' '))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
