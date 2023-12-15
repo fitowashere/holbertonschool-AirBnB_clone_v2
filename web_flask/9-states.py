6a3ac7fd92ba60945bd936966edc083893aed880
fitowashere <fito_washere@icloud.com>
2023-12-15 17:33:17 -0400

    task 10 and validation

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" This script that starts a Flask web application """
+""" starts a Flask web application """
 
 from flask import Flask, render_template
 from models import storage
