ef60118e8bb757b5412760d0f57de625d70025ee
fitowashere <fito_washere@icloud.com>
2023-12-15 10:56:51 -0400

    validation

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -48,7 +48,7 @@ def number_template(n):
 def number_odd_or_even(n):
     """creates a route of /number_odd_or_even"""
     odd_or_even = 'even' if n % 2 == 0 else 'odd'
-    return render_template('6-number_odd_or_even.html', n=n, odd_or_even=o_e
+    return render_template('6-number_odd_or_even.html', n=n, odd_or_even=o_e)
 
 
 if __name__ == '__main__':
