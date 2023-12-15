3abadc9b1f40ac449d69a1462ecb052f597137e2
fitowashere <fito_washere@icloud.com>
2023-12-15 11:04:10 -0400

    validation

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,12 @@ def hello_hbnb():
     """ Create a route for home"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Create a route of /hbnb"""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -11,15 +11,18 @@ def hello_hbnb():
     """ Create a route for home"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Create a route of /hbnb"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """ Create a route of /c"""
     return 'C {}'.format(text.replace('_', ' '))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
